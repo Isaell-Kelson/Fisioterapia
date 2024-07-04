1df73fb78d9fad5de9c7ab29461477ffcbc23c78
Isaell Kelson <isaellkelson@gmail.com>
2024-07-04 17:51:40 -0300

    adjust components

--- src/app/shared/components/apresentation/apresentation.component.ts
+++ src/app/shared/components/apresentation/apresentation.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Component} from '@angular/core';
 import {NgClass, NgOptimizedImage} from "@angular/common";
 
 @Component({
@@ -12,21 +12,5 @@ import {NgClass, NgOptimizedImage} from "@angular/common";
   styleUrl: './apresentation.component.css'
 })
 export class ApresentationComponent {
-  isVisible = false;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const section = this.el.nativeElement.querySelector('.apresentation__section');
-    const boundingClientRect = section.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-
-    // Verifica se a seção está visível no viewport
-    if (boundingClientRect.top >= 0 && boundingClientRect.bottom <= windowHeight) {
-      this.isVisible = true;
-    } else {
-      this.isVisible = false;
-    }
-  }
 }

d564611aff49cff63070729375887fe25a9ed731
Isaell Kelson <isaellkelson@gmail.com>
2024-07-24 08:35:08 -0300

    Adjusted the map animation to only happen when the section is in the viewport

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,43 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 import * as L from 'leaflet';
-
 
 L.Icon.Default.imagePath = 'src/assets/';
 
 @Component({
   selector: 'app-map',
-  standalone: true,
-  imports: [],
   templateUrl: './map.component.html',
+  standalone: true,
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
   private map!: L.Map;
 
   ngOnInit(): void {
-    this.initMap();
+  }
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.initMap();
+          observer.disconnect();
+        }
+      });
+    });
+
+    const section = document.querySelector('#map-section');
+    if (section) {
+      observer.observe(section);
+    }
   }
 
   private initMap(): void {
-
     this.map = L.map('map', {
       center: [-8.8137, -36.9541],
       zoom: 7,
       zoomControl: false
     });
 
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
-
 
     setTimeout(() => {
       this.map.flyTo([-8.047562, -34.877001], 13, {
         animate: true,
         duration: 2
       });
-
 
       setTimeout(() => {
         L.marker([-8.047562, -34.877001], {
